5faa69fc4c8e44fd931ae43936b6b74c3b44edc4
Rahul Rhoades <rhoadesrahul@gmail.com>
2024-09-27 14:11:09 -0600

    Breaking out files into terminal module

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,8 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::event::{ read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers };
+use crossterm::execute;
+use crossterm::terminal::{ disable_raw_mode, enable_raw_mode, Clear, ClearType, size };
+use crossterm::cursor::{ MoveTo };
+use std::io::stdout;
 
 pub struct Editor {
     should_quit: bool,
@@ -11,31 +14,71 @@ impl Editor {
     }
 
     pub fn run(&mut self) {
-        if let Err(err) = self.repl() {
-            panic!("{err:#?}");
-        }
-        print!("Goodbye. \r\n");
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
     }
 
-    pub fn repl(&mut self) -> Result<(), std::io::Error> {
+    fn initialize() -> Result<(), std::io::Error> {
         enable_raw_mode()?;
-        loop {
-            if let Key( KeyEvent {
-                            code, modifiers, kind, state
-                        }) = read()? {
-                println!("Code {code:?} Modifier {modifiers:?} Kind {kind:?} State {state:?} \r");
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-                    }
-                    _ => ()
-                }
-                if self.should_quit {
-                    break;
-                }
+        Self::clear_screen()
+    }
+
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Self::clear_screen()?;
+            print!("Goodbye.\r\n");
+        } else {
+            self.draw_rows()?;
+            MoveTo(0, 0);
+        }
+        Ok(())
+    }
+
+    fn draw_rows(&self) -> Result<(), std::io::Error>{
+        let size = size()?;
+        for current_row in 0..size.1 {
+            print!("~");
+            if current_row + 1 < size.1 {
+                print!("\r\n");
             }
         }
-        disable_raw_mode()?;
         Ok(())
+    }
+
+    fn repl(&mut self) -> Result<(), std::io::Error> {
+        loop {
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+            let event = read()?;
+            self.evaluate_event(&event);
+        }
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+                       code, modifiers, ..
+                   }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                _ => (),
+            }
+        }
     }
 }
